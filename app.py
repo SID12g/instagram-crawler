8dea469ee0010d23b7a7b58991ce9523b190cfbe
SID12g <sid12g@naver.com>
2024-02-21 00:07:36 +0900

    fix: if not exist image (ex. video, error..), can skip

--- app.py
+++ app.py
@@ -28,25 +28,37 @@ e.send_keys(Keys.ENTER)
 # move to tag = '사과'
 time.sleep(4)
 driver.get('https://www.instagram.com/explore/tags/%EC%82%AC%EA%B3%BC/')
+
+# driver.get('https://www.instagram.com/p/C2ujnmyvL3v/') # for video skip test
+
 driver.implicitly_wait(10) # max wait = 10sec (better than time.sleep)
 e = driver.find_elements(By.CSS_SELECTOR, '._aagw')[0].click()
 
 def getImage(i): 
     driver.implicitly_wait(10)
-    image = driver.find_element(By.CSS_SELECTOR, '._aa1_ ._aa20 ._aagv .xu96u03').get_attribute('src')
-    if(image):
+    try:
+        image = driver.find_element(By.CSS_SELECTOR, '._aa1_ ._aa20 ._aagv .xu96u03').get_attribute('src')
         print(i, image)
         urllib.request.urlretrieve(image, f'{i}.jpg')
-    # if next page doesn't have image, click next button
-    else: 
+        e = driver.find_element(By.CSS_SELECTOR, '._aaqg ._abl-')
+        driver.execute_script('arguments[0].click();', e)
+        time.sleep(1)
+    except:
         print('not img!!')
         e = driver.find_element(By.CSS_SELECTOR, '._aaqg ._abl-')
         driver.execute_script('arguments[0].click();', e)
-
-    # click next button
-    e = driver.find_element(By.CSS_SELECTOR, '._aaqg ._abl-')
-    driver.execute_script('arguments[0].click();', e)
-    time.sleep(1)
+        time.sleep(1)
 
 for i in range(10):
     getImage(i)
+        
+# driver.implicitly_wait(10)
+# try:
+#     image = driver.find_element(By.CSS_SELECTOR, '._aa1_ ._aa20 ._aagv .xu96u03').get_attribute('src')
+#     print(image, type(image))
+#     urllib.request.urlretrieve(image, 'test.jpg')
+#     print('end store image process')
+
+# except: print('fail')
+    
+# print('end all process')
